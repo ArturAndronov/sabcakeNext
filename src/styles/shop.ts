de8fe047d568b4f3a51f7d39002ed8c5216ab886
Artur <artik_andronov@mail.ru>
2023-07-23 11:44:57 +0300

    add sortPopup component and togglePopup

--- src/styles/shop.ts
+++ src/styles/shop.ts
@@ -168,57 +168,7 @@ export const ShopContainer = styled("div")`
     align-items: center;
     justify-content: space-between;
   }
-  .sort {
-    position: relative;
-
-    &__label {
-      display: flex;
-
-
-      svg {
-        margin-right: 8px;
-      }
-
-      b {
-        margin-right: 8px;
-      }
-
-      span {
-        color: #b1ca31;
-        border-bottom: 1px dashed #b1ca31;
-        cursor: pointer;
-      }
-    }
-    &__popup {
-      position: absolute;
-      right: 0;
-      margin-top: 15px;
-      background: #fff;
-      box-shadow: 0px 5px 15px rgba(0,0,0,0.09);
-      border-radius: 10px;
-      overflow: hidden;
-      padding: 10px 0;
-      width: 150px;
-
-      ul{
-        overflow: hidden;
-        li{
-          padding: 12px 20px;
-          cursor: pointer;
-
-          &.active,
-          &:hover {
-            background: rgba(254, 95, 30, 0.05);
-          }
-
-          &.active {
-            font-weight: bold;
-            color: #b1ca31;
-          }
-        }
-      }
-    }
-  }
+  
 
   .content__items{
     display: flex;
